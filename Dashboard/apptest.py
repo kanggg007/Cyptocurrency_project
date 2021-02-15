20e9745ed90d1001b64cd3ae053b49b02cf9a3e2
Your Name <tw9ch@hotmail.com>
2021-02-14 22:24:42 -0500

    dashboard -

--- Dashboard/apptest.py
+++ Dashboard/apptest.py
@@ -28,8 +28,8 @@ from keras import Sequential
 from keras.layers import LSTM
 from keras.layers import Dense
 from sklearn.preprocessing import MinMaxScaler
-
-
+from statsmodels.tsa.arima_model import ARIMA
+from sklearn.preprocessing import StandardScaler
 # data =  pd.read_csv('test_data')
 
 # from config import db_password
@@ -92,14 +92,8 @@ for factor in factors:
 
   
 # Model loading  
-    # loaded_model = joblib.load('../LSTM_models/BTC_LSTM.h5')
-    # result = as
-    # new_data = pd.read_csv('test_data')[['time','name','close']]
-    # new_data =new_data.loc[data['time'].between(start_date, end_date)]
-coin_df = data.loc[(data['name']=='Bitcoin')]
-coin_df = coin_df.dropna()
 coin_df = coin_df[['time', 'close']].copy()
-    
+coin_df.index=coin_df['time']        
     
 cl = coin_df.close.astype('float32')
 train = cl[0:int(len(cl)*0.80)]
@@ -107,8 +101,6 @@ scl = MinMaxScaler()
         #Scale the data
 scl.fit(train.values.reshape(-1,1))
 cl =scl.transform(cl.values.reshape(-1,1))
-    #Create a function to process the data into lb observations look back slices
-    # and create the train test dataset (90-10)
 def processData(coin_df,lb):
     X,Y = [],[]
     for i in range(len(coin_df)-lb-1):
@@ -123,18 +115,10 @@ y_train,y_test = y[:int(y.shape[0]*0.90)],y[int(y.shape[0]*0.90):]
 path_lstm = 'LSTM_models/ETH_LSTM.h5'
 model = load_model(path_lstm)
     
-# model = Sequential()
-# model.add(LSTM(256,input_shape=(lb,1)))
-# model.add(Dense(1))
-# model.compile(optimizer='adam',loss='mse')
-    # #Reshape data for (Sample,Timestep,Features) 
+
 X_train = X_train.reshape((X_train.shape[0],X_train.shape[1],1))
 X_test = X_test.reshape((X_test.shape[0],X_test.shape[1],1))
-    #Fit model with history to check for overfitting
 model.fit(X_train,y_train,epochs=100,validation_data=(X_test,y_test),shuffle=False)
-    # model.summary() 
-
-    # # plt.figure(figsize=(12,8))
 Xt = model.predict(X_train)
 list1 = scl.inverse_transform(y_test.reshape(-1,1)).tolist()
 list2 = scl.inverse_transform(Xt).tolist()
@@ -147,6 +131,72 @@ genral_fig6.add_trace(go.Scatter(y=test_merge['Predicted'], x=test_merge.index, 
 
 genral_fig6.add_trace(go.Scatter(y=test_merge['Actual'], x=test_merge.index, name="Actual"),secondary_y=True,)
 
+# # Arima Model
+coin_df = data.loc[(data['name']=='Bitcoin')]
+coin_df = coin_df[['time', 'close']].copy()
+coin_df.index=coin_df['time']
+df_close = coin_df['close']
+df_log = np.log(df_close)
+train_data, test_data = df_log[3:int(len(df_log)*0.9)], df_log[int(len(df_log)*0.9):]
+
+# path_a = 'ARIMA_models/BTC_ARIMA.h5'
+# model_a = joblib.load(path_a)
+
+
+model_a = ARIMA(train_data, order=(1, 1, 1))  
+fitted = model_a.fit(disp=-1)  
+
+fc, se, conf = fitted.forecast(72, alpha=0.05)  # 95% confidence
+fc_series = pd.Series(fc, index=test_data.index)
+test_data=pd.DataFrame(test_data)
+train_data=pd.DataFrame(train_data)
+fc_series=pd.DataFrame(fc_series)
+
+genral_fig7 = make_subplots(specs=[[{"secondary_y": False}]])
+genral_fig7.add_trace(
+    go.Scatter(y=test_data['close'], x=test_data.index, name="Actual Price"),
+    secondary_y=False,
+    )
+
+genral_fig7.add_trace(
+    go.Scatter(y=train_data['close'], x=train_data.index, name="Training"),
+    secondary_y=False,
+    )
+genral_fig7.add_trace(
+    go.Scatter(y=fc_series[0], x=fc_series.index, name="Predicted Price"),
+    secondary_y=False,
+    )
+
+# #Random Forest
+# new_data = data.loc[data['name'] == 'Bitcoin']
+# new_data = new_data.dropna()
+# # new_data.drop('Unnamed: 0', axis=1, inplace= True)
+# target = new_data['close']
+# inputs = new_data[['url_shares','reddit_posts','tweets','news','youtube']].copy()
+
+# # Create a StandardScaler instance
+# scaler = StandardScaler()
+# # Fit the StandardScaler
+# input_scaler = scaler.fit(inputs)
+# input_scaled = input_scaler.transform(inputs)
+#     #predicted value
+# # path_b = 'DF_models/BTC_RF.HDF5'
+
+# # load_model = joblib.load(path_a)
+# y_pred = load_model.predict(input_scaled)
+# prices_df =pd.DataFrame(list(zip(y_pred,target)), columns=['Predicted', 'Actual'])
+# fig8 = go.Figure()
+# fig8.add_trace(
+#         go.Scatter(
+#             y=prices_df['Actual']
+#         ))
+# fig8.add_trace(
+#         go.Scatter(
+#             y=prices_df['Predicted']
+#         ))
+
+
+
 # setup layout
 app.layout = html.Div([
                     html.H1(
@@ -158,7 +208,9 @@ app.layout = html.Div([
                     html.Div([dcc.Graph(id='data-plot-overview', figure=genral_fig)], className='row3'),
                     html.Div([dcc.Graph(id='data-plot-overview2', figure=genral_fig2)], className='row4'),
                     html.Div([dcc.Graph(id='data-plot-overview3', figure=genral_fig3)], className='row5'),
-                    html.Div([dcc.Graph(id='data-plot-overview4', figure=genral_fig6)], className='row6'),
+                    # html.Div([dcc.Graph(id='data-plot-overview4', figure=genral_fig6)], className='row6'),
+                    html.Div([dcc.Graph(id='data-plot-overview5', figure=genral_fig7)], className='row7'),
+                    # html.Div([dcc.Graph(id='data-plot-overview6', figure=fig8)], className='row86'),
                     html.H3(
                         children = 'Select Cryptocurrency',
                         style = {
@@ -368,74 +420,113 @@ def update_social3(selected_ticket):
     return genral_fig5
 
 @app.callback(
+    Output(component_id='k', component_property='figure'),
+    [
+    # Input(component_id='date-picker-range', component_property='start_date'),
+    # Input(component_id='date-picker-range', component_property='end_date'),
+    Input(component_id='my-dropdown', component_property='value')]
+    )
+def arima():
+#
+    coin_df = data.loc[(data['name']=='Bitcoin')]
+    coin_df = coin_df[['time', 'close']].copy()
+    coin_df.index=coin_df['time']
+    df_close = coin_df['close']
+    df_log = np.log(df_close)
+    train_data, test_data = df_log[3:int(len(df_log)*0.9)], df_log[int(len(df_log)*0.9):]
+
+    path_a = 'ARIMA_models/BTC_ARIMA.h5'
+    model_a = joblib.load(path_a)
+
+
+    # model_a.fit(X_train,y_train,epochs=100,validation_data=(X_test,y_test),shuffle=False)
+    # model_a = ARIMA(train_data, order=(1, 1, 1))  
+    # fitted = model_a.fit(disp=-1)  
+
+
+    model_a = ARIMA(train_data, order=(1, 1, 1))  
+    fitted = model_a.fit(disp=-1)  
+
+    fc, se, conf = fitted.forecast(72, alpha=0.05)  # 95% confidence
+    fc_series = pd.Series(fc, index=test_data.index)
+    test_data=pd.DataFrame(test_data)
+    train_data=pd.DataFrame(train_data)
+    fc_series=pd.DataFrame(fc_series)
+
+    genral_fig7 = make_subplots(specs=[[{"secondary_y": False}]])
+    genral_fig7.add_trace(
+        go.Scatter(y=test_data['close'], x=test_data.index, name="Actual Price"),
+        secondary_y=False,
+        )
+
+    genral_fig7.add_trace(
+        go.Scatter(y=train_data['close'], x=train_data.index, name="Training"),
+        secondary_y=False,
+        )
+    genral_fig7.add_trace(
+        go.Scatter(y=fc_series[0], x=fc_series.index, name="Predicted Price"),
+        secondary_y=False,
+        )
+#  def update_LSTM(selected_ticket):
+#     coin_df = data.loc[data['symbol']== selected_ticket]
+#     coin_df = coin_df[['time', 'close']].copy()
+        
+        
+#     cl = coin_df.close.astype('float32')
+#     train = cl[0:int(len(cl)*0.80)]
+#     scl = MinMaxScaler()
+#             #Scale the data
+#     scl.fit(train.values.reshape(-1,1))
+#     cl =scl.transform(cl.values.reshape(-1,1))
+#         #Create a function to process the data into lb observations look back slices
+#         # and create the train test dataset (90-10)
+#     def processData(coin_df,lb):
+#         X,Y = [],[]
+#         for i in range(len(coin_df)-lb-1):
+#             X.append(coin_df[i:(i+lb),0])
+#             Y.append(coin_df[(i+lb),0])
+#         return np.array(X),np.array(Y)
+#     lb=10
+#     X,y = processData(cl,lb)
+#     X_train,X_test = X[:int(X.shape[0]*0.90)],X[int(X.shape[0]*0.90):]
+#     y_train,y_test = y[:int(y.shape[0]*0.90)],y[int(y.shape[0]*0.90):]
+        
+#     path_lstm = 'LSTM_models/'+selected_ticket+'_LSTM.h5'
+#     model = load_model(path_lstm)
+        
+#     # model = Sequential()
+#     # model.add(LSTM(256,input_shape=(lb,1)))
+#     # model.add(Dense(1))
+#     # model.compile(optimizer='adam',loss='mse')
+#         # #Reshape data for (Sample,Timestep,Features) 
+#     X_train = X_train.reshape((X_train.shape[0],X_train.shape[1],1))
+#     X_test = X_test.reshape((X_test.shape[0],X_test.shape[1],1))
+#         #Fit model with history to check for overfitting
+#     model.fit(X_train,y_train,epochs=100,validation_data=(X_test,y_test),shuffle=False)
+#         # model.summary() 
+
+#         # # plt.figure(figsize=(12,8))
+#     Xt = model.predict(X_train)
+#     list1 = scl.inverse_transform(y_test.reshape(-1,1)).tolist()
+#     list2 = scl.inverse_transform(Xt).tolist()
+#     test=pd.DataFrame(list1)
+#     test2=pd.DataFrame(list2)   
+#     test_merge = test.merge(test2, left_index=True, right_index=True)
+#     test_merge.columns=['Predicted', 'Actual']
+#     genral_fig6 = make_subplots(specs=[[{"secondary_y": True}]])
+#     genral_fig6.add_trace(go.Scatter(y=test_merge['Predicted'], x=test_merge.index, name="Predicted"),secondary_y=False,)
+
+#     genral_fig6.add_trace(go.Scatter(y=test_merge['Actual'], x=test_merge.index, name="Actual"),secondary_y=True,)
+
+#     return genral_fig6
+
+@app.callback(
     Output(component_id='l', component_property='figure'),
     [
     # Input(component_id='date-picker-range', component_property='start_date'),
     # Input(component_id='date-picker-range', component_property='end_date'),
     Input(component_id='my-dropdown', component_property='value')]
     )
-
-# def update_LSTM(selected_ticket):
-    # model_imported = tf.keras.models.load_model("../../LSTM_models/BTC_LSTM.H5")
-
-    
-    
-    # loaded_model = joblib.load('../LSTM_models/BTC_LSTM.h5')
-    # # result = as
-    # # new_data = pd.read_csv('test_data')[['time','name','close']]
-    # # new_data =new_data.loc[data['time'].between(start_date, end_date)]
-    # coin_df = data.loc[(data['name']=='Bitcoin')]
-    # coin_df = coin_df.dropna()
-    # coin_df = coin_df[['time', 'close']].copy()
-    
-    
-    # cl = coin_df.close.astype('float32')
-    # train = cl[0:int(len(cl)*0.80)]
-    # scl = MinMaxScaler()
-    #     #Scale the data
-    # scl.fit(train.values.reshape(-1,1))
-    # cl =scl.transform(cl.values.reshape(-1,1))
-    # #Create a function to process the data into lb observations look back slices
-    # # and create the train test dataset (90-10)
-    # def processData(coin_df,lb):
-    #     X,Y = [],[]
-    #     for i in range(len(coin_df)-lb-1):
-    #         X.append(coin_df[i:(i+lb),0])
-    #         Y.append(coin_df[(i+lb),0])
-    #     return np.array(X),np.array(Y)
-    # lb=10
-    # X,y = processData(cl,lb)
-    # X_train,X_test = X[:int(X.shape[0]*0.90)],X[int(X.shape[0]*0.90):]
-    # y_train,y_test = y[:int(y.shape[0]*0.90)],y[int(y.shape[0]*0.90):]
-    
-    # path_lstm = '../LSTM_models/BTC_LSTM.h5'
-    # modle_lstm = load_model(path_lstm)
-
-    # # model = Sequential()
-    # # model.add(LSTM(256,input_shape=(lb,1)))
-    # # model.add(Dense(1))
-    # # model.compile(optimizer='adam',loss='mse')
-    # # #Reshape data for (Sample,Timestep,Features) 
-    # # X_train = X_train.reshape((X_train.shape[0],X_train.shape[1],1))
-    # # X_test = X_test.reshape((X_test.shape[0],X_test.shape[1],1))
-    # #Fit model with history to check for overfitting
-    # modle_lstm.fit(X_train,y_train,epochs=100,validation_data=(X_test,y_test),shuffle=False)
-    # # model.summary() 
-
-    # # # plt.figure(figsize=(12,8))
-    # Xt = modle_lstm.predict(X_train)
-    # list1 = scl.inverse_transform(y_test.reshape(-1,1)).tolist()
-    # list2 = scl.inverse_transform(Xt).tolist()
-    # test=pd.DataFrame(list1)
-    # test2=pd.DataFrame(list2)   
-    # test_merge = test.merge(test2, left_index=True, right_index=True)
-    # test_merge.columns=['Predicted', 'Actual']
-    # genral_fig6 = make_subplots(specs=[[{"secondary_y": True}]])
-    # genral_fig6.add_trace(go.Scatter(y=test_merge['Predicted'], x=test_merge.index, name="Predicted"),secondary_y=False,)
-
-    # genral_fig6.add_trace(go.Scatter(y=test_merge['Actual'], x=test_merge.index, name="Actual"),secondary_y=True,)
-    # return genral_fig6
-
 #  app.layout = html.Div([
 #     dcc.Textarea(
 #         id='textarea-example',
